b2a4df71caf4af6ee98a2c710f6bef416475be7d
Huy Nguyen <giahuy2201@icloud.com>
2024-02-03 14:30:45 -0500

    Update linode

--- app/core/linode.py
+++ app/core/linode.py
@@ -26,41 +26,35 @@ def create_instance(region: str, expiration: int):
         label=instance_label,
         root_pass=config.SSH_PASSWD,
     )
-    instance_dict[instance_label] = new_linode
-    # Start timer
-    if expiration != 0:
-        expiration_dict[instance_label] = datetime.datetime.now() + datetime.timedelta(
-            hours=expiration
-        )
-        # Terminate instance 1 min less than set time to account for delay
-        timer = threading.Timer(expiration * 3540, delete_instance, [instance_label])
-        timer.start()
-        print(
-            "{instance_label} ({instance_ipv4}) timer set for {expiration} hours".format(
-                instance_label=new_linode.label,
-                instance_ipv4=new_linode.ipv4[0],
-                expiration=expiration,
-            )
-        )
-    else:
-        expiration_dict[instance_label] = 0
     if new_linode:
-        print(
-            "{instance_label} ({instance_ipv4}) created".format(
-                instance_label=new_linode.label, instance_ipv4=new_linode.ipv4[0]
+        instance_dict[instance_label] = new_linode
+        instance_ipv4 = instance_ipv4 = new_linode.ipv4[0]
+        print(f"{instance_label} ({instance_ipv4}) created")
+        # Start timer
+        if expiration != 0:
+            expiration_dict[
+                instance_label
+            ] = datetime.datetime.now() + datetime.timedelta(hours=expiration)
+            # Terminate instance 1 min less than set time to account for delay
+            timer = threading.Timer(
+                expiration * 3540, delete_instance, [instance_label]
             )
-        )
+            timer.start()
+            print(
+                f"{instance_label} ({instance_ipv4}) timer set for {expiration} hours"
+            )
+        else:
+            expiration_dict[instance_label] = 0
     return new_linode
 
 
 def delete_instance(instance_label: str):
     instance = instance_dict[instance_label]
-    print(
-        "{instance_label} ({instance_ipv4}) deleted".format(
-            instance_label=instance_label, instance_ipv4=instance.ipv4[0]
-        )
-    )
-    return instance_dict.pop(instance_label).delete()
+    instance_ipv4 = instance_ipv4 = instance.ipv4[0]
+    success = instance_dict.pop(instance_label).delete()
+    if success:
+        print(f"{instance_label} ({instance_ipv4}) deleted")
+    return success
 
 
 # references for created instances
